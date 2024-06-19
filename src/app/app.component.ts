dbdff82063f7ad7e5636398d60d983f3145f282f
joabesnts09 <joabesilva1233@gmail.com>
2024-06-19 15:24:13 -0300

    feat: add form control and responsiveness in style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { RouterOutlet } from '@angular/router'
 import { HeaderComponent } from './components/header/header.component'
 import { FormsComponent } from './components/forms/forms.component'
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AppComponent, RouterOutlet, HeaderComponent, FormsComponent],
+  imports: [
+    AppComponent,
+    RouterOutlet,
+    HeaderComponent,
+    FormsComponent
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,],
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.scss'
+  styleUrl: './forms.component.scss',
 })
 export class FormsComponent {
-  results = [];
+  results = []
+  formGroup!: FormGroup
+  
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.formGroup.value);
+    
+  }
+
+  onClear(): void {
+    this.formGroup.reset()
+    
+  }
 }
